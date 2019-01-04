77b982f0c9797c5732532de4d6fb5c4536cc7feb
Ivar Refsdal <refsdal.ivar@gmail.com>
2019-01-04 14:13:53 +0100

    ..

--- ssb-desilformue-endring.R
+++ ssb-desilformue-endring.R
@@ -10,7 +10,7 @@ mf <- ApiData(url, returnMetaFrames = TRUE)
 
 url <- "http://data.ssb.no/api/v0/no/table/10318"
 df <- ApiData(url, 
-              Tid=c("2017"),
+              Tid=c("2010", "2017"),
               Desiler=TRUE,
               ContentsCode=c("BereknFormue"))
 
@@ -20,15 +20,36 @@ df <- df$`10318: Del av berekna nettoformue, gjennomsnittleg berekna nettoformue
 
 df <- df[, !(names(df) %in% c("statistikkvariabel"))]
 
-ggplot(df, aes(x = reorder(desil, value),
-               y = value)) +
-  expand_limits(y = 325e6) +
+wide <- dcast(df, desil ~ år, value.var="value")
+
+wide$endring = (wide$`2017` - wide$`2010`)
+
+df <- wide
+df <- df[order(df$endring),]
+
+fmt <- function(n) {
+  if (n>=1e6) {
+    return(sprintf("%s.", gsub("\\.", ",", sprintf("%.1f mill", n/1e6))))
+  }
+  return(format(n, digits=9, decimal.mark=",",
+                big.mark=" ",small.mark=".", , small.interval=3))
+}
+
+df$endringMill = df$endring / 1e6
+df$endringReadble = mapply(fmt, df$endring)
+
+ggplot(df, aes(x = reorder(desil, endring),
+               label=endringReadble,
+               y = endringMill)) +
+  expand_limits(y = 125) +
   geom_bar(stat = "identity", width = .75, position = "dodge2") +
+  geom_text(color="black", size=3, position=position_dodge(width=0.9), hjust=-0.15) +
   coord_flip() +
-  labs(title="Gjennomsnittleg berekna nettoformue, 2017",
+  guides(fill = guide_legend(reverse=TRUE)) +
+  labs(title="Endring i gjennomsnittleg nettoformue 2010-2017",
        subtitle="Hushaldningar gruppert etter desil",
        x="Desil",
-       y="Kroner",
+       y="Millionar kroner",
        fill="År",
        caption = "Kjelde: SSB (tabell 10318)\nDiagram: Refsdal.Ivar@gmail.com") +
   scale_y_continuous(labels = scales::comma) +
