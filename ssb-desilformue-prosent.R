a71b87f93b84f2aad3ad80eb61565206ff940f8d
Ivar Refsdal <refsdal.ivar@gmail.com>
2019-02-04 20:21:41 +0100

    More work on desilformue

--- ssb-desilformue-prosent.R
+++ ssb-desilformue-prosent.R
@@ -4,7 +4,6 @@ library(reshape2)
 
 # https://www.ssb.no/inntekt-og-forbruk/statistikker/ifhus/aar
 # https://www.ssb.no/statbank/table/10318
-
 url <- "http://data.ssb.no/api/v0/no/table/10318"
 
 mf <- ApiData(url, returnMetaFrames = TRUE)
@@ -12,36 +11,22 @@ mf <- ApiData(url, returnMetaFrames = TRUE)
 df <- ApiData(url, 
               Tid=c("2017"),
               Desiler=TRUE,
-              ContentsCode=c("BereknFormue"))
-
-df
+              ContentsCode=c("Nettoformue"))
 
 df <- df$`10318: Del av berekna nettoformue, gjennomsnittleg berekna nettoformue og lågaste verdi i desil for hushald, etter desil, statistikkvariabel og år`
 
-df <- df[, !(names(df) %in% c("statistikkvariabel"))]
+df <- df[df$desil != "I alt", ]
 
-fmt <- function(n) {
-  if (n>=1e6) {
-    return(sprintf("%s.", gsub("\\.", ",", sprintf("%.1f mill", n/1e6))))
-  }
-  return(format(n, digits=9, decimal.mark=",",
-                big.mark=" ",small.mark=".", , small.interval=3))
-}
-
-df$vMill = df$value / 1e6
-df$vReadable = mapply(fmt, df$value)
+df
 
 ggplot(df, aes(x = reorder(desil, value),
-               y = vMill,
-               label = vReadable)) +
-  expand_limits(y = 325) +
+               y = value)) +
   geom_bar(stat = "identity", width = .75, position = "dodge2") +
-  geom_text(color="black", size=3, position=position_dodge(width=0.9), hjust=-0.15) +
   coord_flip() +
-  labs(title="Gjennomsnittleg berekna nettoformue, 2017",
-       subtitle="Hushaldningar gruppert etter desil",
+  labs(title="Del av berekna nettoformue",
+       subtitle="Hushaldningar gruppert etter desil. Data for 2017",
        x="Desil",
-       y="Millionar kroner",
+       y="Prosent",
        fill="År",
        caption = "Kjelde: SSB (tabell 10318)\nDiagram: Refsdal.Ivar@gmail.com") +
   scale_y_continuous(labels = scales::comma) +
